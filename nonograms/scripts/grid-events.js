19510a4331d35b5c0de6d04d085cdb5e1f17a1fc
Anastasia Botsula <nactya.botsula@gmail.com>
2024-02-05 11:39:17 +0300

    feat: implement the processing of the winning event

--- nonograms/scripts/grid-events.js
+++ nonograms/scripts/grid-events.js
@@ -1,8 +1,11 @@
+import { templates } from "./templates.js";
 import { generateRowHints, generateColumnHints } from "./hints.js";
 import { createGrid } from "./game-grid.js";
 
 export let currentTemplate = "Camel";
 let oldTemplate = currentTemplate;
+
+let userSolution = [];
 
 export function changeTemplate() {
   document.querySelectorAll(".level-name").forEach((el) => {
@@ -10,6 +13,7 @@ export function changeTemplate() {
       oldTemplate = currentTemplate;
       currentTemplate = el.firstChild.textContent.replace(" ", "_");
       if (currentTemplate !== oldTemplate) {
+        userSolution.length = 0;
         const oldGrid = document.querySelector(".grid-wrapper");
         oldGrid.remove();
         generateRowHints();
@@ -27,6 +31,7 @@ export function fillCells() {
     cell.addEventListener("click", (event) => {
       cell.classList.remove("--negative");
       cell.classList.toggle("--positive");
+      fillUserSolution();
     });
     cell.addEventListener("contextmenu", (event) => {
       event.preventDefault();
@@ -34,4 +39,30 @@ export function fillCells() {
       cell.classList.toggle("--negative");
     });
   });
+}
+
+function fillUserSolution() {
+  const gridWidth = templates[currentTemplate].width;
+  const gridHeight = templates[currentTemplate].height;
+  const solution = templates[currentTemplate].solution;
+
+  userSolution = Array(gridHeight)
+    .fill()
+    .map(() => Array(gridWidth).fill(0));
+
+  const positiveCells = document.querySelectorAll(".--positive");
+
+  positiveCells.forEach((el) => {
+    const columnInd = el.parentElement.getAttribute("data-column");
+    const rowInd = el.parentElement.getAttribute("data-row");
+    userSolution[rowInd][columnInd] = 1;
+  });
+
+  gameOver(solution, userSolution);
+}
+
+function gameOver(sol, userSol) {
+  if (sol.toString() === userSol.toString()) {
+    alert("You win!");
+  }
 }
